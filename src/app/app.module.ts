8df0344dc2ce53e3b28be3e565730efe234bd026
Jared Wight <JWight1@1800contacts.com>
2021-11-19 17:44:09 -0700

    simplifies and fixes logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IncomeFormComponent } from './components/income-form/income-form.component';
 import { TooltipComponent } from './components/tooltip/tooltip.component';
-import { JointComponent } from './components/joint/joint.component';
-import { SingleComponent } from './components/single/single.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IncomeFormComponent,
-    TooltipComponent,
-    JointComponent,
-    SingleComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule, ReactiveFormsModule
-  ],
+  declarations: [AppComponent, IncomeFormComponent, TooltipComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/tax.service.ts
+++ src/app/services/tax.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { map, tap } from 'rxjs/operators';
-import {
-  Bracket,
-  calculateTotalTax,
-  createBracketArray,
-} from '../models/Bracket';
+import { Bracket } from '../models/Bracket';
 import { IIncome, InfoService } from './info.service';
 
 const JointBracketsArray = [
@@ -21,13 +17,50 @@ const JointBracketsArray = [
 })
 export class TaxService {
   private jointTaxBrackets2021: Bracket[] =
-    createBracketArray(JointBracketsArray);
+    this.createBracketArray(JointBracketsArray);
   constructor(private readonly infoService: InfoService) {}
   public TotalTax$ = this.infoService.incomes$.pipe(
-    map(
-      (x: IIncome): number =>
-        calculateTotalTax(this.jointTaxBrackets2021, x.primary) +
-        calculateTotalTax(this.jointTaxBrackets2021, x.secondary)
+    map((x: IIncome): number =>
+      this.calculateTotalTax(this.jointTaxBrackets2021, x.primary + x.secondary)
     )
   );
+
+  private createBracketArray(brackets: number[][]): Bracket[] {
+    const newBrackets: Bracket[] = [];
+
+    for (let i = 0; i < brackets.length; i++) {
+      const current = brackets[i];
+      const next = brackets[i + 1];
+      let min = 0;
+
+      if (next) {
+        min = next[0] + 0.01;
+      }
+
+      newBrackets.push(new Bracket(current[0], current[1], min));
+    }
+
+    return newBrackets;
+  }
+
+  private calculateTotalTax(brackets: Bracket[], grossIncome: number): number {
+    if (!grossIncome) {
+      return 0;
+    }
+    const filtered = brackets.filter((x) => {
+      console.log(x.min, grossIncome);
+      return x.min <= grossIncome;
+    });
+    console.log(filtered);
+    const total = filtered.reduce((sum, cur) => {
+      if (cur.max < grossIncome) {
+        sum += cur.total;
+        console.log(cur.total);
+      } else {
+        sum += cur.taxPercent * 0.01 * (grossIncome - cur.min + 0.01);
+      }
+      return sum;
+    }, 0);
+    return total;
+  }
 }
